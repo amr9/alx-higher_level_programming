2e9f5f5a75a1c9a8a91351540a38745f7acf5597
Amr Emad <amr12201147@gmail.com>
2023-11-29 15:40:50 -0800

    scripts of python

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * check_cycle - checks if a singly linked list has a cycle in it
+ * @list: pointer to the head of the list
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
+int check_cycle(listint_t *list)
+{
+    listint_t *slow, *fast;
+
+    while (list != NULL && list->next != NULL)
+    {
+        slow = list;
+        fast = list->next;
+
+        if (slow == fast)
+            return (1);
+
+        list = list->next;
+    }
+
+    return (0);
+}
