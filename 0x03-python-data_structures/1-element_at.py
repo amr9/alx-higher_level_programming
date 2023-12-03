a12b5ea205039a768a5b81ab37b928ae68c3fcf8
Amr Emad <amr12201147@gmail.com>
2023-12-03 08:33:07 -0800

    scripts from 1 to 6

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    list_length = len(my_list) - 1
+
+    if idx < 0:
+        return None
+    elif idx > list_length:
+        return None
+    else:
+        return my_list[idx]
