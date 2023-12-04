44a8798ab7ee9866be52d80cb572fa5513df125a
Amr Emad <amr12201147@gmail.com>
2023-12-04 08:17:39 -0800

    errors

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,14 +1,14 @@
 #include "lists.h"
 /**
- * palindrome - checks if a singly linked list is a palindrome
+ * is_palindrome - checks if a singly linked list is a palindrome
  *@head: head list
  *Return: 0 if its not a palindrome, 1 if it is a palindrome
  */
-int palindrome(listint_t **head)
+int is_palindrome(listint_t **head)
 {
-	if (head == NULL || *head == NULL)
-		return (1);
-	return (aux_palindrome(head, *head));
+    if (head == NULL || *head == NULL)
+        return (1);
+    return (aux_palindrome(head, *head));
 }
 
 /**
@@ -19,12 +19,12 @@ int palindrome(listint_t **head)
  */
 int aux_palindrome(listint_t **head, listint_t *end)
 {
-	if (end == NULL)
-		return (1);
-	if (aux_palindrome(head, end->next) && (*head)->n == tail->n)
-	{
-		*head = (*head)->next;
-		return (1);
-	}
-	return (0);
+    if (end == NULL)
+        return (1);
+    if (aux_palindrome(head, end->next) && (*head)->n == tail->n)
+    {
+        *head = (*head)->next;
+        return (1);
+    }
+    return (0);
 }
