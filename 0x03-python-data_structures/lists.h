0d32cb3573da3583145954469d1751bc908ea6cd
Amr Emad <amr12201147@gmail.com>
2023-12-04 08:12:23 -0800

    error

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,5 +1,8 @@
 #ifndef LISTS_H
 #define LISTS_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
  * struct listint_s - singly linked list
@@ -19,6 +22,7 @@ size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 
+int aux_palindrome(listint_t **head, listint_t *end);
 int is_palindrome(listint_t **head);
 
 #endif /* LISTS_H */
