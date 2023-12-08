be4fc46bc345b5801a242c64b652424b8ba4c0f0
Amr Emad <amr12201147@gmail.com>
2023-12-08 15:25:31 -0800

    pycode error

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,7 +4,8 @@ def roman_to_int(roman_string):
         return 0
     total = 0
     integer = 0
-    roman_digits = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    roman_digits = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+                    'C': 100, 'D': 500, 'M': 1000}
     for x in reversed(roman_string):
         integer = roman_digits[x]
         total += integer if total < integer * 5 else -integer
